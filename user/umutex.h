40eb6b1719ac209abdc1d0b00d53c709794ae6f5
Zifeng Huang <zhuang61@hawk.illinoistech.edu>
2026-04-09 19:39:24 -0400

    Added user files to xv6 folder and editted MakeFile

--- user/umutex.h
+++ user/umutex.h
@@ -1,6 +1,7 @@
 #pragma once
-
-typedef struct { volatile int locked; } umutex_t;
+typedef struct {
+int locked;
+} umutex_t;
 void mutex_init(umutex_t *m);
 void mutex_lock(umutex_t *m);
 void mutex_unlock(umutex_t *m);
